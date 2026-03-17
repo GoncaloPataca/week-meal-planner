7ee6d35d12d2a1c6733729058f6807530a60ed6b
Gonçalo Pataca <gonzalo.nascimento.pataca@gmail.com>
2026-03-17 21:37:44 +0000

    chore: add vite-plugin-mkcert and enable HTTPS for dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [react(), mkcert()],
+  server: {
+    https: true,
+    host: true
+  },
   build: {
     outDir: 'docs'
   }
